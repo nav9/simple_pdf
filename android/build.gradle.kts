78794f9bbba27665015b7f8e57ba5820621e9a99
Navin <navinipe@gmail.com>
2026-01-15 00:03:38 +0530

    attempting to fix the issue of Android crashes but not working

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,6 +14,19 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+
+    afterEvaluate {
+        if (project.plugins.hasPlugin("com.android.library") || project.plugins.hasPlugin("com.android.application")) {
+            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
+            if (android.namespace == null) {
+                val namespace = when (project.name) {
+                    "pdf_manipulator" -> "com.deepanshuchaudhary.pdf_manipulator"
+                    else -> "com.nav.simplepdf.patch.${project.name.replace("_", ".")}"
+                }
+                android.namespace = namespace
+            }
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
